217982255f899c574b2b2d40a95b4ec68f9322bd
Nahom <101549562+nahom-d54@users.noreply.github.com>
2024-05-16 20:01:50 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,4 +9,13 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
+python manage.py makemigrations
 python manage.py migrate
+
+email="$DJANGO_SUPERUSER_EMAIL"
+phone="$DJANGO_SUPERUSER_PHONE"
+password="$DJANGO_SUPERUSER_PASSWORD"
+username="$DJANGO_SUPERUSER_USERNAME"
+
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$email', '$password', phone_number='$phone', username='$username')" | python manage.py shell
