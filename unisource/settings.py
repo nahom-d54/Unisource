e6a102e0e43fa27796c09f74c93ccc4cce26f503
Nahom <101549562+nahom-d54@users.noreply.github.com>
2024-05-16 21:15:39 +0300

    Update settings.py

--- unisource/settings.py
+++ unisource/settings.py
@@ -182,7 +182,7 @@ AUTH_USER_MODEL = 'users.UserUnisource'
 
 #if not DEBUG:
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # STORAGES = {
 #     "default": {
