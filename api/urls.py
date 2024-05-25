8d1b75e83a17ef90016c825f67deabd111f39726
Nahom_d54 <nahom.d54@gmail.com>
2024-05-25 16:40:10 +0300

    fix: made rating individual routes

--- api/urls.py
+++ api/urls.py
@@ -13,7 +13,7 @@ router = DefaultRouter()
 router.register('category', views.CategoryViewSet,basename='category')
 router.register('resource', views.ResourceViewset, basename='resource')
 router.register('review', views.ReviewViewset, basename='review')
-router.register('rating', views.RatingViewset, basename='rating')
+#router.register('rating', views.RatingViewset, basename='rating')
 
 
 urlpatterns = [
@@ -22,5 +22,7 @@ urlpatterns = [
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('register/', views.RegisterView.as_view(), name='register_user'),
+    path('rating/<int:resource>/', views.RatingViewset.as_view({'get': 'list', 'post': 'create'}), name='rating-list'),
+    path('rating/<int:resource>/<int:pk>/', views.RatingViewset.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='rating-detail'),
 ]
 
