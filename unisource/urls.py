81aaefc19dc4b49ba607389326697bdb285f310f
Nahom_d54 <nahom.d54@gmail.com>
2024-05-25 10:02:12 +0300

    feat: added rest framework ath page

--- unisource/urls.py
+++ unisource/urls.py
@@ -41,6 +41,7 @@ urlpatterns = [
     path('api/v1/', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('auth/', include('rest_framework.urls'))
 ]
 
 if settings.DEBUG:
