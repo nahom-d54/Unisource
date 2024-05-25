9434a2ecf93d5c667bbefaefe85246d1266bb3c9
Nahom <101549562+nahom-d54@users.noreply.github.com>
2024-05-25 11:23:43 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,10 +12,10 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
-email="$DJANGO_SUPERUSER_EMAIL"
-phone="$DJANGO_SUPERUSER_PHONE"
-password="$DJANGO_SUPERUSER_PASSWORD"
-username="$DJANGO_SUPERUSER_USERNAME"
+# email="$DJANGO_SUPERUSER_EMAIL"
+# phone="$DJANGO_SUPERUSER_PHONE"
+# password="$DJANGO_SUPERUSER_PASSWORD"
+# username="$DJANGO_SUPERUSER_USERNAME"
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$email', '$password', phone_number='$phone', username='$username')" | python manage.py shell
+# echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$email', '$password', phone_number='$phone', username='$username')" | python manage.py shell
 
